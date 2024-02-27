214ea5a8bb0acb71482286378a8068b23e6e5ae3
deepakkr14 <deepakbipin142@gmail.com>
2024-02-28 01:20:44 +0530

    added about us route

---
+++ pages/aboutus.js
@@ -0,0 +1,5 @@
+function AboutUs(){
+    return <h2>hell i am about us  page</h2>
+}
+
+export default AboutUs;
---
+++ pages/news.js
@@ -0,0 +1,5 @@
+function NewsPage(){
+    return <h2>hell i am news page page</h2>
+}
+
+export default NewsPage;
