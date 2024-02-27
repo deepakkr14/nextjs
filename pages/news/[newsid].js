cf79a627fd1a155c82721d67f04fd5913e58b73a
deepakkr14 <deepakbipin142@gmail.com>
2024-02-28 01:54:13 +0530

    added dynamic routes with dummy data

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,19 +1,29 @@
 //  dynamic rutes
-import {useRouter} from 'next/router'
-function Dynamic(){
-    const router = useRouter()
-    const newsid=  router.query.newsid
-    const details = [
+import { useRouter } from "next/router";
+const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
 
-        { id : 1, name: 'Yash', role: 'Senior Developer'},
-        
-        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
-        
-        { id : 3, name: 'Suresh', role: 'Frontend Developer'}
-        
-        ]
-    return <><h2>hello i am dynamic routes </h2><h5>got the data of {details[newsid]}
-    {details[newsid].name} and role of  {details[newsid].role}</h5></>
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+function Dynamic() {
+  const router = useRouter();
+  const newsid = router.query.newsid;
+  const validid=details.findIndex(x => x.id == newsid)
+ const validation=validid != -1 ? true : false
+  return (
+    <>
+      <h2>hello i am dynamic routes </h2>
+     {validation && <h5>
+        got the data of {details[newsid]}
+        {details[newsid].name} and role of {details[newsid].role}
+      </h5>} 
+     {!validation && <h5>
+       no Developer found
+      </h5>} 
+    </>
+  );
 }
 
 export default Dynamic;
