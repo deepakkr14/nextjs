a2e8f0123751fdcd8e6839e22f0eac9243734058
deepakkr14 <deepakbipin142@gmail.com>
2024-02-28 13:42:52 +0530

    completing about us page updated

--- pages/aboutus/[id].js
+++ pages/aboutus/[id].js
@@ -1,37 +1,28 @@
 //  dynamic rutes
 import { useRouter } from "next/router";
-const details = [
+function Developer() {
+  const details = [
     { id: 1, name: "Yash", role: "Senior Developer" },
 
     { id: 2, name: "Vaibhav", role: "Backend Developer" },
 
     { id: 3, name: "Suresh", role: "Frontend Developer" },
   ];
-function Developer() {
   const router = useRouter();
   const id = router.query.id;
-  const validid=details.findIndex(x => x.id == id)
- const validation=validid != -1 ? true : false
+  const detail = details.find((x) => x.id == id);
   return (
-    <>
-      <h2>hello i am dynamic routes </h2>
-     {validation && <h5>
-        got the data of {details[id]}
-        {details[id].name} and role of {details[id].role}
-      </h5>} 
-     {!validation && <h5>
-       no Developer found
-      </h5>} 
-    </>
+    <div>
+      {detail ? (
+        <div>
+          <h1>{detail.name}</h1>
+          <p>{detail.role}</p>
+        </div>
+      ) : (
+        <p>Developer doesn't exist</p>
+      )}
+    </div>
   );
 }
 
 export default Developer;
-
-
-// function Developer(){
-//     return <h2>hell i am deepak kumar</h2>
-
-// }
-
-// export default Developer;
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,18 +1,26 @@
-import {Link} from 'next/link'
+import Link from "next/link";
 const details = [
-    { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 1, name: "Yash", role: "Senior Developer" },
 
-    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
 
-    { id: 3, name: "Suresh", role: "Frontend Developer" },
-  ];
-function AboutUs(){
-    return<> <h2>hell i am about us  page</h2>
-    <h3>These are the developers</h3>
-    <Link href="/aboutus/1">Yash </Link>
-    <Link href="/aboutus/2">Vaibhav </Link>
-    <Link href="/aboutus/3">Suresh </Link>
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+function AboutUs() {
+  return (
+    <>
+      <h2>hell i am about us page</h2>
+      <h3>These are the developers</h3>
+
+      <ul>
+        {details.map((each) => (
+          <li>
+            <Link href={`/aboutus/${each.id}`}> {each.name}</Link>
+          </li>
+        ))}
+      </ul>
     </>
+  );
 }
 
 export default AboutUs;
